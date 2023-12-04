6e6f89bfb556bc22f778d2e338ce72ef9e76c246
Maza455 <same7nte@gmail.com>
2023-12-04 14:09:24 +0200

    Adding home page with a contact.html, but is not yet finished

--- Functionality/load.js
+++ Functionality/load.js
@@ -1,1 +1,10 @@
 // Loading page functionality
+$(document).ready(function() {
+    load();
+    
+function load(){
+    setTimeout(function(){
+        $('.loader').addClass('loaded');
+    }, 3000);
+    }
+});
--- Functionality/script.js
+++ Functionality/script.js
@@ -1,1 +1,20 @@
 // E-commerce Functionality
+
+// Banner Owl Carousel
+$(document).ready(() => {
+$('#owl_carousel .owl-carousel').owlCarousel({
+    items: 1,
+    dots:true,
+    loop: true,
+    autoplay:true,
+    autoplayTimeout:8000,
+    stagePadding: 0
+});
+});
+
+// scroll to to button
+
+const btnScrollToTop = document.querySelector("#btnScrollToTop");
+    btnScrollToTop.addEventListener("click", () => {
+        window.scrollTo(0, 0);
+});
