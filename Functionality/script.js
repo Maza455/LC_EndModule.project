4b3b7650d610a42acba94c426eec426028fafab4
Maza455 <same7nte@gmail.com>
2023-12-05 10:57:30 +0200

    Adding checkout page but not yet finished

--- Functionality/script.js
+++ Functionality/script.js
@@ -19,3 +19,19 @@ const btnScrollToTop = document.querySelector("#btnScrollToTop");
     btnScrollToTop.addEventListener("click", () => {
         window.scrollTo(0, 0);
 });
+
+
+// Add to Cart
+$('.select-ui').change(function(){
+    let value=parseInt($(this).parent('div').parent('div').parent('div').children('.col-md-2').children('h5').children('span').text());
+    let total= parseInt($(this).val()) * value;
+    console.log($(this).parent('div').parent('div').parent('div').children('.col-md-2').children('h5').children('span').text());
+    $(this).parent('div').parent('div').parent('div').children('.col-md-2').children('h5').children('span').text(total.toString());
+    let totalvalue=0;
+    $('.select-ui').each(function(){
+      totalvalue+=parseInt($(this).parent('div').parent('div').parent('div').children('.col-md-2').children('h5').children('span').text());
+    });
+    $('#total').text(totalvalue.toString());
+    totalvalue+=100;
+    $('#totaltopay').text(totalvalue.toString());
+  });
