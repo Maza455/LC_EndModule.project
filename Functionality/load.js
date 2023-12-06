1c2c1d4f5cc396dfdab01efaa6c65aa6f0b80d4f
Maza455 <same7nte@gmail.com>
2023-12-06 17:12:46 +0200

    Adding checkout updates and search input for products

--- Functionality/load.js
+++ Functionality/load.js
@@ -1,10 +1,21 @@
 // Loading page functionality
-$(document).ready(function() {
-    load();
+// $(document).ready(function() {
+//     load();
     
-function load(){
-    setTimeout(function(){
-        $('.loader').addClass('loaded');
-    }, 3000);
+// function load(){
+//     setTimeout(function(){
+//         $('.loader').addClass('loaded');
+//     }, 3000);
+//     }
+// });
+
+// Loading page functionality
+document.addEventListener('DOMContentLoaded', function() {
+    load();
+  
+    function load() {
+      setTimeout(() => {
+        document.querySelector('.loader').classList.add('loaded');
+      }, 3000);
     }
-});
+  });
